bb73cadc46fb94fa57372159168d71a6c64829eb
Natnael Solomon <natnaels844@gmail.com>
2026-09-05 06:41:51 -0300

    Fix Firebase configuration exports for authentication

--- src/firebase.ts
+++ src/firebase.ts
@@ -10,6 +10,7 @@ const firebaseConfig = {
   appId: '1:242812226177:web:12da1db1e243159fbc1b80',
 };
 
+export const firebaseConfigured = Object.values(firebaseConfig).every(Boolean);
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
