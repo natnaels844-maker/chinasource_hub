4bd93371a089a7964e60bed8ef752083092034cc
Natnael Solomon <natnaels844@gmail.com>
2026-09-08 13:16:50 -0300

    Include new sourcing product catalog

--- src/products.ts
+++ src/products.ts
@@ -4,5 +4,6 @@ import {productData3} from './product-data-3';
 import {productData4} from './product-data-4';
 import {productData5} from './product-data-5';
 import {productData6} from './product-data-6';
+import {productData7} from './product-data-7';
 export type Product={id:number;name:string;category:string;price:string;moq:string;image:string;source:string;unlock:number;bigDeal?:boolean};
-export const products:Product[]=[...productData1,...productData2,...productData3,...productData4,...productData5,...productData6];
+export const products:Product[]=[...productData1,...productData2,...productData3,...productData4,...productData5,...productData6,...productData7];
